6e6ad942ac94922b70990edefb972349ee60622d
anaickgithub <anaickm@gmail.com>
2017-02-15 12:44:25 +0100

    exo parentheses

--- recursivity/src/main/scala/recursivity/Parenthesis.scala
+++ recursivity/src/main/scala/recursivity/Parenthesis.scala
@@ -3,7 +3,37 @@ package recursivity
 import scala.annotation.tailrec
 
 object Parenthesis {
-  
-  def balance(chars: List[Char]): Boolean =
-    ??? // TODO : implement here the algorith to check if the parenthesis are balanced
+
+  def balance(chars: List[Char], compteur: Int = 0): Boolean = {
+    // TODO : implement here the algorith to check if the parenthesis are balanced
+
+    /*
+    //println(s"$chars $compteur")
+    if (compteur < 0) {
+      //println("comteur<0")
+      false
+    } else {
+      if (chars.isEmpty && compteur == 0) {
+        //println("comteur==0 et chars vide")
+        true
+      } else {
+        if (chars.head == '(') {
+          balance(chars.tail, compteur + 1)
+        } else {
+          if (chars.head == ')') {
+            balance(chars.tail, compteur - 1)
+          } else {
+            balance(chars.tail,compteur)
+          }
+        }
+      }
+    }*/
+
+    if (compteur < 0) false
+    else if (chars.isEmpty && compteur == 0) true
+    else if (chars.head == '(') balance(chars.tail, compteur + 1)
+    else if (chars.head == ')') balance(chars.tail, compteur - 1)
+    else balance(chars.tail, compteur)
+
+  }
 }
